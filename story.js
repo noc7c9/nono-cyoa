00e5b4835bb0c512d1e3c5c524256699ff1edab9
NONOCuber <125516460+NONOCuber@users.noreply.github.com>
2023-03-17 20:55:55 -0500

    ask function push
    the ask function is ready to be fully tested

--- story.js
+++ story.js
@@ -1,100 +1,99 @@
 (async () => {
     hello = await ask(
-        'Hello I am your master, you want to start your training right?',
+        'Hello, I am your master. I heard you want to start your training right?',
         ['yes', 'no'],
     );
 
     if (hello == 'yes') {
         print();
         ays = await ask(
-            'You know that if you complete your training you have to, and I mean have to unless you to die. So I ask again do you want to?',
+            'You know that if you complete your training you have to finish your missions, and I mean have to! Unless you to die. So I ask again do you want to?',
             ['yes', 'no'],
         );
 
         if (ays == 'yes') {
             print();
             role = await ask(
-                'First thing first, what would you like to be',
+                'First thing first, what would you like to be?',
                 ['fighter', 'defender', 'magician'],
             );
 
-            //             if (role != 'fighter' && ays != 'defender' && ays != 'magician') {
-            //                 redo_question();
-            //                 ays = await input(
-            //                     'You know that if you complete your training you have to, and I mean have to unless you to die. So I ask again do you want to? (yes/no) ',
-            //                 );
-            //             }
-
             if (role == 'defender') {
                 print();
-                print('it is a boring job and you defend the base until you get killed');
+                print('It is a boring job. You defend the base until you get killed');
                 print('(end)');
             }
 
             if (role == 'fighter') {
                 print();
-                weapon = await input('what weapon would you like (bow/sword)');
+                weapon = await ask('What weapon would you like?',
+                    ['bow', 'sword'],
+                );
 
                 if (weapon == 'sword') {
                     print();
-                    print('go practice with the dummies');
+                    print('Go practice with the dummies.');
                     print();
-                    print('you get the the practice course');
+                    print('You get the the practice course.');
                     print(
-                        'you have praciticed for a week you are ready for your quest',
+                        'You have praciticed for a week, you are ready for your quest.',
                     );
                     print();
                     print(
-                        'you embark on your quest with only instructions and 3 coins',
+                        'You embark on your quest with only instructions and 3 coins.',
                     );
                     print();
-                    where = await input(
-                        'where would you like to go (hotel 2 coins/camp 0 coins/shop ? coins)',
+                    where = await ask(
+                        'Where would you like to go?',
+                        ['hotel', 'camp', 'shop'],
                     );
 
                     if (where == 'camp') {
                         print();
                         print(
-                            'you go camping and you are scared because you have no materials',
+                            'You go camping. You become scared, you have no materials.',
                         );
-                        print('a bear finds an easy meal by eating you');
+                        print('A bear finds you as an easy meal.');
                         print('(end)');
                     }
 
                     if (where == 'shop') {
                         print();
-                        print('you are in the shop');
-                        camping_gear = await input(
-                            'do you want camping gear (yes/no)',
+                        print('You are in the shop.');
+                        camping_gear = await ask(
+                            'do you want camping gear?', 
+                            ['yes', 'no'],
                         );
 
                         if (camping_gear == 'no') {
                             print();
                             print(
-                                'you did not buy from my shop!!! you will perish',
+                                'You did not buy from my shop!!! You will perish.',
                             );
                             print('(end)');
 
                             if (camping_gear == 'yes') {
                                 print();
-                                print('it is one coin');
-                                again_place = await input(
-                                    'now where do you want to go (camping 0 coins/hotel 2 coins)',
+                                print('The camping gear is one coin.');
+                                again_place = await ask(
+                                    'Now where do you want to go?',
+                                    ['camping', 'hotel'],
                                 );
 
                                 if (again_place == 'camping') {
                                     print();
                                     print(
-                                        'a bear comes to your camp but you are able to defend yourself',
+                                        'A bear comes to your camp, but you are able to defend yourself.',
                                     );
-                                    scroll = await input(
-                                        'Do you want to read instructions (yes/no)',
+                                    scroll = await ask(
+                                        'Do you want to read the instructions?',
+                                        ['yes', 'no'],
                                     );
 
                                     if (scroll == 'no') {
                                         print();
                                         print(
-                                            'you throw the instructions aside and a fox comes and tears it up you are lost forever',
+                                            'You throw the instructions aside. A fox comes and tears them up, you are lost forever.',
                                         );
                                         print('(end)');
                                     }
@@ -102,26 +101,27 @@
                                     if (scroll == 'yes') {
                                         print();
                                         print();
-                                        print('Quest Instructions');
+                                        print('Quest Instructions:');
                                         print();
                                         print(
-                                            'You must retrieve the gold from the safe in megalotroplolis the city owes us a big debt',
+                                            'You must retrieve the gold from the safe in Megalotroplolis, the city owes us a big debt.',
                                         );
                                         print(
-                                            'if you fail you will be executed',
+                                            'If you fail you will be executed.',
                                         );
                                         print();
-                                        follow = await input(
-                                            'do you want to stay for the next night (yes/no)',
+                                        follow = await ask(
+                                            'Do you want to stay for the next night?', 
+                                            ['yes', 'no'],
                                         );
 
                                         if (follow == 'yes') {
                                             print();
                                             print(
-                                                'you are caught in a big storm and are hit in the head with hail and it bleeds so fast you dont have time to thik before you faint',
+                                                'You are caught in a big storm. You are hit in the head with hail, it bleeds so fast you dont have time to think before you faint.',
                                             );
                                             print(
-                                                'when you wake up you do not know who you are',
+                                                'When you wake up you do not know who you are.',
                                             );
                                             print('(end)');
                                         }
@@ -129,30 +129,32 @@
                                         if (follow == 'no') {
                                             print();
                                             print(
-                                                'you are in megalotropolis you set up camp',
+                                                'You are in Megalotropolis you set up camp.',
                                             );
-                                            coin = await input(
-                                                'you find 2 coins on the ground one is heads and one is tails which one do you want to pick up (heads/tails)',
+                                            coin = await ask(
+                                                'You find 2 coins on the ground, one is facing heads and one is facing tails, which one do you want to pick up?
+                                                ['heads', 'tails'],
                                             );
 
                                             if (coin == 'heads') {
                                                 print();
                                                 print(
-                                                    'you pick up the coin with heads it is cursed a monster comes out and murders you',
+                                                    'You pick up the coin with heads, it is cursed a monster comes out and murders you.',
                                                 );
                                                 print('(end)');
                                             }
 
                                             if (coin == 'tails') {
                                                 print();
-                                                traveler = await input(
-                                                    'there is a wandering traveler he wants to stay with you (yes/no)',
+                                                traveler = await ask(
+                                                    'There is a wandering traveler, he wants to stay with you. DO you allow him?',
+                                                    ['yes', 'no'],
                                                 );
 
                                                 if (traveler == 'no') {
                                                     print();
                                                     print(
-                                                        'the traveler gets mad at you and while you are sleeping commites arsen on your tent',
+                                                        'The traveler gets mad at you. While you are sleeping commites arson on your tent.',
                                                     );
                                                     print('(end)');
                                                 }
@@ -160,19 +162,20 @@
                                                 if (traveler == 'yes') {
                                                     print();
                                                     print(
-                                                        'He thanks you for your generosity',
+                                                        'He thanks you for your generosity.',
                                                     );
                                                     print();
-                                                    print('(it is now night)');
+                                                    print('(It is now night.)');
                                                     print();
-                                                    satchel = await input(
-                                                        'The man is now asleep do you want to search the satchel? (yes/no)',
+                                                    satchel = await ask(
+                                                        'The man is now asleep do you want to search the satchel?' 
+                                                        ['yes','no'],
                                                     );
 
                                                     if (satchel == 'yes') {
                                                         print();
                                                         print(
-                                                            'The man is not actually asleep he reaches over and kills you',
+                                                            'The man is not actually asleep he reaches over and kills you.',
                                                         );
                                                         print('(end)');
                                                     }
@@ -180,7 +183,7 @@
                                                     if (satchel == 'no') {
                                                         print();
                                                         print(
-                                                            'the man is evil and kills you in your sleep',
+                                                            'The man is evil and kills you in your sleep.',
                                                         );
                                                         print('(end)');
                                                     }
@@ -193,7 +196,7 @@
                                 if (again_place == 'hotel') {
                                     print();
                                     print(
-                                        'you are in the hotel and there is a shooting you save someones life by jumping in front of the bullet',
+                                        'You are in the hotel. There is a shooting, you save someones life by jumping in front of the bullet.',
                                     );
                                     print('(end)');
                                 }
@@ -204,7 +207,7 @@
                     if (where == 'hotel') {
                         print();
                         print(
-                            'you are in the hotel and there is a shooting you save someones life by jumping in front of the bullet',
+                            'You are in the hotel. There is a shooting, you save someones life by jumping in front of the bullet.',
                         );
                         print('(end)');
                     }
@@ -212,30 +215,32 @@
 
                 if (weapon == 'bow') {
                     print();
-                    print('now go to practice in the archery range');
+                    print('Now go to practice in the archery range!');
+                    print('No more story here for now!');
                 }
             }
 
             if (role == 'magician') {
                 print();
-                print('ok you get one week of traning');
+                print('Ok, you get one week of traning.');
                 print();
-                win = input(
-                    'the master calls you to his office do you go (yes/no)',
+                win = ask(
+                    'The master calls you to his office do you go?',
+                    {'yes' 'no'],
                 );
 
                 if (win == 'yes') {
                     print();
                     print(
-                        "you go to the office and the master tells you 'you will die you will become to powerful!!!!",
+                        'You go to the office. The master tells you, "you must die, if ot you will become to powerful!!!"',
                     );
-                    print('you teleport away');
+                    print('You teleport away.');
                     print();
-                    print('2 years later');
+                    print('(2 years later)');
                     print();
-                    print('you teleport back to the place and kill the master');
+                    print('You teleport back to the castle and kill the master.');
                     print(
-                        'you request to not be the new master but to live your life in peace',
+                        'You request to not be the new master, but to live your life in peace.',
                     );
                     print();
                     print('CONGRATS!!!');
@@ -247,14 +252,14 @@
 
         if (ays == 'no') {
             print();
-            print('then get out of here!');
+            print('Then get out of here!');
             print('(end)');
         }
     }
 
     if (hello == 'no') {
         print();
-        print('alright now be on your way');
+        print('Alright, now be on your way');
         print('(end)');
     }
 })();
